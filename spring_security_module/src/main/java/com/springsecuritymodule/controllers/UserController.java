3365ac9d4fbda4640acdb3fcd2205ef2e2d2518d
Sandy <sandy.chuchuca@gmail.com>
2014-09-29 21:44:28 +0200

    Correcta configuración de webmvc-config.xml. Funciona correctamente el gestor de vistas con .html. Funcionan correctamente las llamadas a controladores.

--- spring_security_module/src/main/java/com/springsecuritymodule/controllers/UserController.java
+++ spring_security_module/src/main/java/com/springsecuritymodule/controllers/UserController.java
@@ -1,11 +1,33 @@
 package com.springsecuritymodule.controllers;
 import com.springsecuritymodule.entities.User;
+
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RequestMapping("/users")
 @Controller
 @RooWebScaffold(path = "users", formBackingObject = User.class)
 public class UserController {
+	
+	@RequestMapping(value ="/welcome")
+	public String welcomePage(Model model) {
+		System.out.println("UserController - welcomePage()");
+		model.addAttribute("title", "Spring Security Hello World");
+		model.addAttribute("message", "This is welcome page!");
+		return "hello";
+ 
+	}
+ 
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
+	public String adminPage(Model model) {
+		System.out.println("UserController - adminPage()");
+		model.addAttribute("title", "Spring Security Hello World");
+		model.addAttribute("message", "This is protected page!");
+ 
+		return "admin";
+ 
+	}
 }
